7a59c97004eb2cdd4844138d3d77e0720aad943d
Emily Renegar <egrenegar@gmail.com>
2020-06-08 19:42:35 -0400

    updated schema and seeds to remove manager

--- db/schema.sql
+++ db/schema.sql
@@ -18,6 +18,5 @@ CREATE TABLE employee (
 id INTEGER PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
-role_id INTEGER,
-manager_id INTEGER
+role_id INTEGER
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -12,11 +12,11 @@ VALUES ('Sales Lead', 100000, 1),
 ('Senior Analyst', 90000, 4),
 ('Junior Analyst', 65000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Jeff', 'Hoffman', 3, null),
-('Emily', 'Renegar', 2, 1),
-('John', 'Smith', 2, 1),
-('Jackson', 'Browne', 1, null),
-('Keyshia', 'Cole', 4, null),
-('Lauren', 'Speed', 5, null),
-('Emma', 'Johnson', 6, 6);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Jeff', 'Hoffman', 3),
+('Emily', 'Renegar', 2),
+('John', 'Smith', 2),
+('Jackson', 'Browne', 1),
+('Keyshia', 'Cole', 4),
+('Lauren', 'Speed', 5),
+('Emma', 'Johnson', 6);
